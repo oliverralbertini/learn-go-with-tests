7be066bbeb028f4af22d77d99821bf5b3b31d8cc
Oliver Albertini <oalbertini@pivotal.io>
2018-12-01 12:59:18 -0800

    Make dictionary a wrapper around map

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,5 +1,15 @@
 package dictionary
 
-func Search(dict map[string]string, search string) string {
-	return dict[search]
+import "errors"
+
+var ErrNotFound = errors.New("could not find that word")
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(search string) (string, error) {
+	definition, ok := d[search]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -3,16 +3,37 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is a test"}
+	dictionary := Dictionary{"test": "this is a test"}
+	t.Run("known word", func(t *testing.T) {
+		key := "test"
+		got, _ := dictionary.Search(key)
+		want := "this is a test"
 
-	key := "test"
-	got := Search(dictionary, key)
-	want := "this is a test"
+		assertStrings(t, want, got, key)
+	})
 
-	assertStrings(t, want, got, key)
+	t.Run("unknown word", func(t *testing.T) {
+		key := "tist"
+		_, err := dictionary.Search(key)
+		want := ErrNotFound
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertError(t, want, err, key)
+	})
 }
 
 func assertStrings(t *testing.T, want, got, given string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got '%s' want '%s' given '%s'", got, want, given)
+	}
+}
+
+func assertError(t *testing.T, want, got error, given string) {
 	t.Helper()
 
 	if got != want {
