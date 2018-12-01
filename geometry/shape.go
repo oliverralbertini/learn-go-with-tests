9bf36a41f5959093a2475b5d7ae7cb1660c15e9a
Oliver Albertini <oalbertini@pivotal.io>
2018-11-30 20:18:09 -0800

    Add in a triangle test

--- geometry/shape.go
+++ geometry/shape.go
@@ -16,6 +16,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (r *Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
@@ -24,10 +29,19 @@ func (c *Circle) Perimeter() float64 {
 	return 2 * math.Pi * c.Radius
 }
 
+// Perimeter assumes an isosceles triangle
+func (t *Triangle) Perimeter() float64 {
+	return t.Base + 2.0*math.Sqrt(t.Height*t.Height+t.Base*t.Base/4.0)
+}
+
 func (r *Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
 func (c *Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
+}
+
+func (t *Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
--- geometry/shape_test.go
+++ geometry/shape_test.go
@@ -5,48 +5,44 @@ import (
 	"testing"
 )
 
-func compareFloats(t *testing.T, got, want float64) {
+func compareFloats(t *testing.T, shape Shape, got, want float64) {
 	t.Helper()
 
 	if want != got {
-		t.Errorf("wanted '%.2f' got '%.2f'", want, got)
+		t.Errorf("#%v wanted '%.2f' got '%.2f'", shape, want, got)
 	}
+}
+
+type ShapeTests []struct {
+	shape   Shape
+	want    float64
+	subject string
 }
 
 func TestPerimeter(t *testing.T) {
-	checkPerim := func(t *testing.T, shape Shape, want float64) {
-		got := shape.Perimeter()
-		compareFloats(t, got, want)
+	perimTests := ShapeTests{
+		{shape: &Rectangle{Width: 1.0, Height: 2.0}, want: 6.0, subject: "perimeter of rectangle"},
+		{shape: &Circle{Radius: 0.5}, want: math.Pi, subject: "perimeter of circle"},
+		{shape: &Triangle{Base: 1.0, Height: math.Sqrt(3) / 2.0}, want: 3.0, subject: "perimeter of triangle"},
 	}
 
-	t.Run("Rectangle perimeter", func(t *testing.T) {
-		rect := Rectangle{1.0, 2.0}
-
-		checkPerim(t, &rect, 6.0)
-	})
-
-	t.Run("Circle perimeter", func(t *testing.T) {
-		circ := Circle{0.5}
-
-		checkPerim(t, &circ, math.Pi)
-	})
+	for _, pt := range perimTests {
+		t.Run(pt.subject, func(t *testing.T) {
+			compareFloats(t, pt.shape, pt.shape.Perimeter(), pt.want)
+		})
+	}
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		got := shape.Area()
-		compareFloats(t, got, want)
+	areaTests := ShapeTests{
+		{shape: &Rectangle{Width: 1.0, Height: 2.0}, want: 2.0, subject: "area of rectangle"},
+		{shape: &Circle{Radius: 1.0}, want: math.Pi, subject: "area of circle"},
+		{shape: &Triangle{Base: 1.0, Height: 2.0}, want: 1.0, subject: "area of triangle"},
 	}
 
-	t.Run("Rectangle area", func(t *testing.T) {
-		rect := Rectangle{1.0, 2.0}
-
-		checkArea(t, &rect, 2.0)
-	})
-
-	t.Run("Circle area", func(t *testing.T) {
-		circ := Circle{1.0}
-
-		checkArea(t, &circ, math.Pi)
-	})
+	for _, at := range areaTests {
+		t.Run(at.subject, func(t *testing.T) {
+			compareFloats(t, at.shape, at.shape.Area(), at.want)
+		})
+	}
 }
