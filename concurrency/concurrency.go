514c6f8fdd3f7c6abc55d35566951ce23310a5e2
Oliver Albertini <oalbertini@pivotal.io>
2018-12-03 10:49:52 -0800

    Add concurrency package

---
+++ concurrency/concurrency.go
@@ -0,0 +1,19 @@
+package concurrency
+
+import "fmt"
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+
+	return results
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
